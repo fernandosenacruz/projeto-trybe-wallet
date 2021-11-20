6469523bb720dc0f38ac3bc1bb9d76b931e96ead
Fernando Sena <fernandosenacruz@gmail.com>
2021-11-20 15:32:58 -0300

    ajuste lint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
@@ -5,31 +6,21 @@ class Wallet extends React.Component {
   constructor() {
     super();
     this.state = {
-      currencies: [],
-      expenses: [],
+      // currencies: [],
+      // expenses: [],
     };
   }
 
-  componentDidMount() {
-
-  }
+  componentDidMount() {}
 
   render() {
     const { userEmail } = this.props;
     return (
       <>
         <header data-testid="email-field">
-          { userEmail }
-          <span
-            data-testid="total-field"
-          >
-            0
-          </span>
-          <span
-            data-testid="header-currency-field"
-          >
-            BRL
-          </span>
+          {userEmail}
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
         </header>
         {/* <select name="selecti">
           <option
@@ -43,6 +34,10 @@ class Wallet extends React.Component {
     );
   }
 }
+
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   userEmail: state.user.email,
