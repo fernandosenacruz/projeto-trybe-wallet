95d945840470c71961b27c9bea0e2342763a75f6
Fernando Sena <fernandosenacruz@gmail.com>
2021-11-20 18:16:47 -0300

    requisito 3 ok

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -22,14 +22,59 @@ class Wallet extends React.Component {
           <span data-testid="total-field">0</span>
           <span data-testid="header-currency-field">BRL</span>
         </header>
-        {/* <select name="selecti">
-          <option
-            data-testid="header-currency-field"
-            value={}
-          >
-            ximira
-          </option>
-        </select> */}
+        <form>
+          <fieldset>
+            <label htmlFor="input-expense">
+              Despesa
+              <input
+                data-testid="value-input"
+                type="number"
+                name="expense"
+                value={  }
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="input-description-expense">
+              Descrição
+              <input
+                data-testid="description-input"
+                type="text"
+                name="description"
+                value={  }
+                onChange={ }
+              />
+            </label>
+            <select
+              name="currency"
+              data-testid="currency-input"
+            >
+              <option>xelo</option>
+            </select>
+            <select
+              name="method"
+              data-testid="method-input"
+            >
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+            <select
+              name="tag"
+              data-testid="tag-input"
+            >
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte' e 'Saúde</option>
+            </select>
+            <button
+              type="button"
+              onClick={ this.handleClick }
+            >
+              Adicionar despesa
+            </button>
+          </fieldset>
+        </form>
       </>
     );
   }
